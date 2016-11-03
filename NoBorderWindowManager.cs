9e6034f791aba54d2a4704a54d9f0e9a66568ff2
serafeimKak <serkakaes@gmail.com>
2016-11-03 11:29:37 +0200

    add windsor

---
+++ NoBorderWindowManager.cs
@@ -0,0 +1,28 @@
+﻿using Caliburn.Micro;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace Iap
+{
+   internal class NoBorderWindowManager:WindowManager
+    {
+        protected override Window CreateWindow(
+          object rootModel,
+          bool isDialog,
+          object context,
+          IDictionary<string, object> settings)
+        {
+            return base.CreateWindow(
+                rootModel,
+                isDialog,
+                context,
+                new Dictionary<string, object>
+                {
+                    { "WindowStyle", WindowStyle.None },
+                    { "WindowState", WindowState.Maximized },
+                    { "WindowStartupLocation", WindowStartupLocation.CenterScreen },
+                    { "ResizeMode", ResizeMode.NoResize }
+                });
+        }
+    }
+}
---
+++ Windsor/AppBootstrapper.cs
@@ -0,0 +1,92 @@
+﻿using Caliburn.Micro;
+using Castle.Windsor;
+using Castle.MicroKernel.Registration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows;
+using System.Threading;
+
+namespace Iap.Windsor
+{
+   internal class AppBootstrapper: BootstrapperBase
+    {
+        private WindsorContainer container;
+
+        public AppBootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            this.container = new WindsorContainer();
+            this.container.AddFacility<EventRegistrationFacility>();
+            this.container.Kernel.Resolver.AddSubResolver(
+                new AppSettingsConvention());
+
+            this.container.Install(
+                Castle.Windsor.Installer.Configuration.FromAppConfig());
+            this.container.Register(
+                Component
+                    .For<CultureInfo>()
+                    .Instance(new CultureInfo("el-GR")),
+                // Component
+                //    .For<MapsBackground>(),
+                Component
+                    .For<IWindowManager>()
+                    .ImplementedBy<NoBorderWindowManager>(),
+                Component
+                    .For<IEventAggregator>()
+                    .ImplementedBy<EventAggregator>(),
+                Classes
+                    .FromThisAssembly()
+                    .BasedOn<PropertyChangedBase>()
+                    .LifestyleTransient(),
+                Classes
+                    .FromThisAssembly()
+                    .Where(x => x.Name.EndsWith("Service"))
+                    .WithServiceDefaultInterfaces(),
+                Classes
+                    .FromThisAssembly()
+                    .Where(x => x.Name.EndsWith("Command"))
+                    .WithServiceDefaultInterfaces(),
+                Classes
+                    .FromThisAssembly()
+                    .Where(x => x.Name.EndsWith("Query"))
+                    .WithServiceDefaultInterfaces(),
+                Classes
+                    .FromThisAssembly()
+                    .Where(x => x.Name.EndsWith("Projection"))
+                    .WithServiceDefaultInterfaces());
+
+            base.Configure();
+        }
+
+        protected override object GetInstance(Type serviceType, string key)
+        {
+            if (string.IsNullOrEmpty(key))
+                return this.container.Resolve(serviceType);
+            return this.container.Resolve(key, serviceType);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type serviceType)
+        {
+            return this.container.ResolveAll(serviceType).Cast<object>();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+
+            DisplayRootViewFor<ShellViewModel>();
+
+
+            base.OnStartup(sender, e);
+        }
+    }
+}
